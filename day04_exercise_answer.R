762f4d109be1adb5d562d0bc991908e3983b1873
manuelhaendel <manuelhaendel@t-online.de>
2020-07-23 14:00:29 +0200

    release flux

--- day04_exercise_answer.R
+++ day04_exercise_answer.R
@@ -33,3 +33,23 @@ for(i in 1:3){
   } 
   if(i > 1) lines(time, n_pools[, i], col = i)
 }
+
+
+n_release <- getReleaseFlux(model_out)
+
+for(i in 1:3){
+  if(i == 1){
+    plot(time, n_release[, i],
+         col = i, ylim = range(n_release), type = "l",
+         xlab = "Years", ylab = "Nitrogen release")
+    # axis(side = 1, at=seq(1,1212, by = 12), labels = seq_along(seq(1,1212, by = 12)))
+    legend(x = 40, y = 7, legend = c("Ammonium", "Nitrate", "Plant"), fill = 1:3)
+  } 
+  if(i > 1) lines(time, n_release[, i], col = i)
+}
+
+
+
+
+
+
